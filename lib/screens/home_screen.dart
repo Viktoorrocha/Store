0497e9e6f9a61468eede4b83b1412e5f933bae74
Viktor Rocha <viktoorrocha@gmail.com>
2018-12-30 15:10:21 -0400

    Barra Flutuante

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:store/tabs/home_tab.dart';
 
 class HomeScreen extends StatelessWidget {
 
@@ -9,9 +10,7 @@ class HomeScreen extends StatelessWidget {
       controller: _pageController,
       physics: NeverScrollableScrollPhysics(),
       children: <Widget>[
-        Container(color: Colors.yellow,),
-        Container(color: Colors.red,),
-        Container(color: Colors.green,)
+        HomeTab()
 
       ],
     );
